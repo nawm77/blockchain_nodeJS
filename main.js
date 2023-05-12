38fef9c912b1858ff3d36e63a6e2344ee3414734
nawm77 <alpha050603@gmail.com>
2023-05-12 23:33:15 +0300

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+// код должен быть выполнен в строгом режиме, что означает, требования при сборке проекта будут повышены,
+// избежать рекомендаций или предупреждений при сборке не получится
+'use strict';
+var CryptoJS = require("crypto-js")
+var express = require("express")
+var bodyParser = require("body-parser")
+var WebSocket = require("ws")
